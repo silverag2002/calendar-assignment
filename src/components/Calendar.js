a8e8478b7eac4aec9970073ce855d855dc92c2da
silverag2002 <silverag2002@gmail.com>
2023-05-03 21:25:10 +0530

    Added next button

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -4,8 +4,8 @@ import dayjs from "dayjs";
 export default function Calendar({ month }) {
   console.log("Month value", month);
   return (
-    <div class="w-screen">
-      <div class="m-5 py-0 flex flex-row-reverse">
+    <React.Fragment>
+      <div className="flex flex-row-reverse">
         <div class="flex items-center">
           <button className="border rounded py-1 px-3 mr-3">Today</button>
         </div>
@@ -27,6 +27,7 @@ export default function Calendar({ month }) {
             </svg>
           </span>
         </button>
+
         <button>
           <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
             <svg
@@ -45,9 +46,6 @@ export default function Calendar({ month }) {
             </svg>
           </span>
         </button>
-        {/* <h2 className="ml-4 text-xl text-gray-500 font-bold">
-          {dayjs(new Date(dayjs().year(), monthIndex)).format("MMMM YYYY")}
-        </h2> */}
       </div>
       <div className="flex-1 grid grid-cols-7 grid-rows-5 ">
         {month.map((row, ind) => (
@@ -57,7 +55,69 @@ export default function Calendar({ month }) {
             ))}
           </React.Fragment>
         ))}
+        {/* </div> */}
       </div>
-    </div>
+    </React.Fragment>
   );
+  // <div class="w-screen">
+  //   <div class="m-5 py-0 ">
+  {
+    /* <div class="flex items-center">
+          <button className="border rounded py-1 px-3 mr-3">Today</button>
+        </div>
+        <button>
+          <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          </span>
+        </button> */
+  }
+  {
+    /* <button>
+          <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 19.5L8.25 12l7.5-7.5"
+              />
+            </svg>
+          </span>
+        </button> */
+  }
+  {
+    /* <h2 className="ml-4 text-xl text-gray-500 font-bold">
+          {dayjs(new Date(dayjs().year(), monthIndex)).format("MMMM YYYY")}
+        </h2> */
+  }
+  //   </div>
+  //   <div className="flex-1 grid grid-cols-7 grid-rows-5 ">
+  //     {month.map((row, ind) => (
+  //       <React.Fragment key={ind}>
+  //         {row.map((day, index) => (
+  //           <Date day={day} key={index} rowIndex={ind} />
+  //         ))}
+  //       </React.Fragment>
+  //     ))}
+  //   </div>
+  // </div>
 }
--- src/components/Date.js
+++ src/components/Date.js
@@ -5,16 +5,16 @@ export default function Date({ day, rowIndex }) {
   console.log("in Date component and date value received", day);
   function designPresentDay() {
     return dayjs().format("DD-MM-YYYY") == day.format("DD-MM-YYYY")
-      ? "bg-blue-600 text-white rounded-full w-8"
+      ? "bg-blue-600 text-white rounded-full w-7"
       : null;
   }
   return (
     <div class="border border-gray-200 flex flex-col">
       <header class="flex flex-col items-center">
         {rowIndex == 0 ? (
-          <p className="txt-sm mt-1">{day.format("ddd").toUpperCase()}</p>
+          <p className="text-sm mt-1">{day.format("ddd").toUpperCase()}</p>
         ) : null}
-        <p class={`txt-sm p-1 my-1 text-center ${designPresentDay()}`}>
+        <p class={`text-sm p-1 my-1 text-center ${designPresentDay()}`}>
           {day.format("DD")}
         </p>
       </header>
